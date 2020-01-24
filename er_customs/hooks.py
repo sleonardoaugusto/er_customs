21ee9a0865e937896c0b2af8d79e2ab4448a7fe8
Leonardo Augusto <sleonardoaugusto@gmail.com>
2020-01-23 23:50:21 -0300

    Hooks, custom fields and workflow added

--- er_customs/hooks.py
+++ er_customs/hooks.py
@@ -2,77 +2,108 @@
 from __future__ import unicode_literals
 from . import __version__ as app_version
 
-app_name = "er_customs"
-app_title = "ER Customs"
-app_publisher = "Leonardo Augusto"
-app_description = "General customizations"
-app_icon = "octicon octicon-file-directory"
-app_color = "grey"
-app_email = "sleonardoaugusto@gmail.com"
-app_license = "MIT"
+app_name = 'er_customs'
+app_title = 'ER Customs'
+app_publisher = 'Leonardo Augusto'
+app_description = 'General customizations'
+app_icon = 'octicon octicon-file-directory'
+app_color = 'grey'
+app_email = 'sleonardoaugusto@gmail.com'
+app_license = 'MIT'
 
 # Includes in <head>
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/er_customs/css/er_customs.css"
-# app_include_js = "/assets/er_customs/js/er_customs.js"
+# app_include_css = '/assets/er_customs/css/er_customs.css'
+# app_include_js = '/assets/er_customs/js/er_customs.js'
 
 # include js, css files in header of web template
-# web_include_css = "/assets/er_customs/css/er_customs.css"
-# web_include_js = "/assets/er_customs/js/er_customs.js"
+# web_include_css = '/assets/er_customs/css/er_customs.css'
+# web_include_js = '/assets/er_customs/js/er_customs.js'
 
 # include js in page
-# page_js = {"page" : "public/js/file.js"}
+# page_js = {'page' : 'public/js/file.js'}
 
 # include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
-# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
-# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
-# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
+# doctype_js = {'doctype' : 'public/js/doctype.js'}
+# doctype_list_js = {'doctype' : 'public/js/doctype_list.js'}
+# doctype_tree_js = {'doctype' : 'public/js/doctype_tree.js'}
+# doctype_calendar_js = {'doctype' : 'public/js/doctype_calendar.js'}
+
+fixtures = [
+    "Property Setter",
+    'Workflow State',
+    'Workflow Action Master',
+    'Workflow',
+    {"dt": "Custom Field",
+     "filters": [[
+         "name", "in", (
+             'Customer-inscricao_estadual',
+             'Customer-nome_fantasia',
+             'Customer-segmento',
+             'Event-link_dinamico',
+             'Event-link_documento',
+             'Item-localizacao',
+             'Lead-contato',
+             'Lead-segmento',
+             'Lead-tipo_calibracao',
+             'Lead-transaction_date',
+             'Lead-uf',
+             'Opportunity-orcamento_link',
+             'Opportunity-segmento',
+             'Opportunity-tipo_calibracao',
+             'Opportunity-uf',
+             'Quotation Item-prazo_de_entrega_em_dias',
+             'Quotation-workflow_state',
+             'Sales Order-emenda_reason',
+         )
+     ]]
+     }
+]
 
 # Home Pages
 # ----------
 
 # application home page (will override Website Settings)
-# home_page = "login"
+# home_page = 'login'
 
 # website user home page (by Role)
 # role_home_page = {
-#	"Role": "home_page"
+#	'Role': 'home_page'
 # }
 
 # Website user home page (by function)
-# get_website_user_home_page = "er_customs.utils.get_home_page"
+# get_website_user_home_page = 'er_customs.utils.get_home_page'
 
 # Generators
 # ----------
 
 # automatically create page for each record of this doctype
-# website_generators = ["Web Page"]
+# website_generators = ['Web Page']
 
 # Installation
 # ------------
 
-# before_install = "er_customs.install.before_install"
-# after_install = "er_customs.install.after_install"
+# before_install = 'er_customs.install.before_install'
+# after_install = 'er_customs.install.after_install'
 
 # Desk Notifications
 # ------------------
 # See frappe.core.notifications.get_notification_config
 
-# notification_config = "er_customs.notifications.get_notification_config"
+# notification_config = 'er_customs.notifications.get_notification_config'
 
 # Permissions
 # -----------
 # Permissions evaluated in scripted ways
 
 # permission_query_conditions = {
-# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# 	'Event': 'frappe.desk.doctype.event.event.get_permission_query_conditions',
 # }
 #
 # has_permission = {
-# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# 	'Event': 'frappe.desk.doctype.event.event.has_permission',
 # }
 
 # Document Events
@@ -80,10 +111,10 @@ app_license = "MIT"
 # Hook on document methods and events
 
 # doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
+# 	'*': {
+# 		'on_update': 'method',
+# 		'on_cancel': 'method',
+# 		'on_trash': 'method'
 #	}
 # }
 
@@ -91,32 +122,31 @@ app_license = "MIT"
 # ---------------
 
 # scheduler_events = {
-# 	"all": [
-# 		"er_customs.tasks.all"
+# 	'all': [
+# 		'er_customs.tasks.all'
 # 	],
-# 	"daily": [
-# 		"er_customs.tasks.daily"
+# 	'daily': [
+# 		'er_customs.tasks.daily'
 # 	],
-# 	"hourly": [
-# 		"er_customs.tasks.hourly"
+# 	'hourly': [
+# 		'er_customs.tasks.hourly'
 # 	],
-# 	"weekly": [
-# 		"er_customs.tasks.weekly"
+# 	'weekly': [
+# 		'er_customs.tasks.weekly'
 # 	]
-# 	"monthly": [
-# 		"er_customs.tasks.monthly"
+# 	'monthly': [
+# 		'er_customs.tasks.monthly'
 # 	]
 # }
 
 # Testing
 # -------
 
-# before_tests = "er_customs.install.before_tests"
+# before_tests = 'er_customs.install.before_tests'
 
 # Overriding Whitelisted Methods
 # ------------------------------
 #
 # override_whitelisted_methods = {
-# 	"frappe.desk.doctype.event.event.get_events": "er_customs.event.get_events"
+# 	'frappe.desk.doctype.event.event.get_events': 'er_customs.event.get_events'
 # }
-
