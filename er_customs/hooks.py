5a2b449b7e075a5fb873f5026ebd189274dfcef0
Leonardo Augusto <sleonardoaugusto@gmail.com>
2020-07-20 22:02:39 -0300

    Get address functionality

--- er_customs/hooks.py
+++ er_customs/hooks.py
@@ -27,6 +27,7 @@ app_license = 'MIT'
 
 # include js in doctype views
 # doctype_js = {'doctype' : 'public/js/doctype.js'}
+doctype_js = {"Address": "public/js/address.js"}
 # doctype_list_js = {'doctype' : 'public/js/doctype_list.js'}
 # doctype_tree_js = {'doctype' : 'public/js/doctype_tree.js'}
 # doctype_calendar_js = {'doctype' : 'public/js/doctype_calendar.js'}
